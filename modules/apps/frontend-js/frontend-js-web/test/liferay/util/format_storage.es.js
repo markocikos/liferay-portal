9e449486e255754634d2b1af4ed623bdc4d2308e
mateomustapic <mateo.mustapic@liferay.com>
2019-06-19 12:54:54 +0200

    LPS-94715 frontend-js-web Test formatStorage utility

---
+++ modules/apps/frontend-js/frontend-js-web/test/liferay/util/format_storage.es.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import formatStorage from '../../../src/main/resources/META-INF/resources/liferay/util/format_storage.es';
+
+describe('Liferay.Util.formatStorage', () => {
+	it('throws error if size parameter is not a number', () => {
+		expect(() => formatStorage({})).toThrow('must be a number');
+	});
+
+	it('should format size under 1048575 bytes to kilobytes, with the default KB suffix', () => {
+		expect(formatStorage(10400)).toEqual('10KB');
+	});
+
+	it('should format size 0 bytes to kilobytes, with the default KB suffix', () => {
+		expect(formatStorage(0)).toEqual('0KB');
+	});
+
+	it('should format size over 1048575 bytes to megabytes, with the default MB suffix', () => {
+		expect(formatStorage(1048576)).toEqual('1.0MB');
+	});
+
+	it('should format size over 1048575 bytes to megabytes with custom space, decimal separator, and suffix type parameters', () => {
+		expect(
+			formatStorage(1048576, {
+				addSpaceBeforeSuffix: true,
+				decimalSeparator: ',',
+				suffixMB: 'megabytes'
+			})
+		).toEqual('1,0 megabytes');
+	});
+});
