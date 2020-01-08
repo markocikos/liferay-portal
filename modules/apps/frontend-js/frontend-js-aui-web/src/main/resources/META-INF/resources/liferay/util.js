d71fe4acc79a203bd1079f0c4597ebfeae8b9c05
Carlos Lancha <carlos.lancha@liferay.com>
2020-01-08 13:26:11 +0100

    LPS-105927 Remove jquery from util/inBrowserView

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -798,31 +798,36 @@
 		},
 
 		inBrowserView(node, win, nodeRegion) {
-			var viewable = false;
+			let viewable = false;
 
-			node = $(node);
+			node = Util.getElement(node);
 
-			if (node.length) {
+			if (node) {
 				if (!nodeRegion) {
-					nodeRegion = node.offset();
+					nodeRegion = node.getBoundingClientRect();
 
-					nodeRegion.bottom = nodeRegion.top + node.outerHeight();
-					nodeRegion.right = nodeRegion.left + node.outerWidth();
+					nodeRegion = {
+						left: nodeRegion.left + window.scrollX,
+						top: nodeRegion.top + window.scrollY
+					};
+
+					nodeRegion.bottom = nodeRegion.top + node.offsetHeight;
+					nodeRegion.right = nodeRegion.left + node.offsetWidth;
 				}
 
 				if (!win) {
 					win = window;
 				}
 
-				win = $(win);
+				win = Util.getElement(win);
 
-				var winRegion = {};
+				const winRegion = {};
 
-				winRegion.left = win.scrollLeft();
-				winRegion.right = winRegion.left + win.width();
+				winRegion.left = win.scrollX;
+				winRegion.right = winRegion.left + win.innerWidth;
 
-				winRegion.top = win.scrollTop();
-				winRegion.bottom = winRegion.top + win.height();
+				winRegion.top = win.scrollY;
+				winRegion.bottom = winRegion.top + win.innerHeight;
 
 				viewable =
 					nodeRegion.bottom <= winRegion.bottom &&
@@ -831,10 +836,15 @@
 					nodeRegion.top >= winRegion.top;
 
 				if (viewable) {
-					var frameEl = $(win.prop('frameElement'));
+					const frameEl = win.frameElement;
 
-					if (frameEl.length) {
-						var frameOffset = frameEl.offset();
+					if (frameEl) {
+						let frameOffset = frameEl.getBoundingClientRect();
+
+						frameOffset = {
+							left: frameOffset.left + window.scrollX,
+							top: frameOffset.top + window.scrollY
+						};
 
 						var xOffset = frameOffset.left - winRegion.left;
 
@@ -848,7 +858,7 @@
 
 						viewable = Util.inBrowserView(
 							node,
-							win.prop('parent'),
+							win.parent,
 							nodeRegion
 						);
 					}
