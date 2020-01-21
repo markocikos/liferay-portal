e1cae352b57b295025ce3a12e01ac63bea4687f0
Carlos Lancha <carlos.lancha@liferay.com>
2020-01-21 10:12:11 +0100

    LPS-98076 wip

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/RichDDMFormFieldType.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/java/com/liferay/dynamic/data/mapping/form/field/type/internal/rich/RichDDMFormFieldType.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.dynamic.data.mapping.form.field.type.internal.color;
+package com.liferay.dynamic.data.mapping.form.field.type.internal.rich;
 
 import com.liferay.dynamic.data.mapping.form.field.type.BaseDDMFormFieldType;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
@@ -21,38 +21,38 @@ import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author Marcela Cunha
+ * @author Carlos Lancha
  */
 @Component(
 	immediate = true,
 	property = {
-		"ddm.form.field.type.data.domain=color",
-		"ddm.form.field.type.description=color-field-type-description",
+		"ddm.form.field.type.data.domain=rich",
+		"ddm.form.field.type.description=rich-field-type-description",
 		"ddm.form.field.type.display.order:Integer=8",
 		"ddm.form.field.type.group=basic", "ddm.form.field.type.icon=adjust",
-		"ddm.form.field.type.label=color-field-type-label",
-		"ddm.form.field.type.name=color", "ddm.form.field.type.scope=app-builder,forms"
+		"ddm.form.field.type.label=rich-field-type-label",
+		"ddm.form.field.type.name=rich", "ddm.form.field.type.scope=app-builder,forms"
 	},
 	service = DDMFormFieldType.class
 )
-public class ColorDDMFormFieldType extends BaseDDMFormFieldType {
+public class RichDDMFormFieldType extends BaseDDMFormFieldType {
 
 	@Override
 	public Class<? extends DDMFormFieldTypeSettings>
 		getDDMFormFieldTypeSettings() {
 
-		return ColorDDMFormFieldTypeSettings.class;
+		return RichDDMFormFieldTypeSettings.class;
 	}
 
 	@Override
 	public String getModuleName() {
-		return "dynamic-data-mapping-form-field-type/ColorPicker" +
-			"/ColorPicker.es";
+		return "dynamic-data-mapping-form-field-type/Rich" +
+			"/Rich.es";
 	}
 
 	@Override
 	public String getName() {
-		return "color";
+		return "rich";
 	}
 
 }
