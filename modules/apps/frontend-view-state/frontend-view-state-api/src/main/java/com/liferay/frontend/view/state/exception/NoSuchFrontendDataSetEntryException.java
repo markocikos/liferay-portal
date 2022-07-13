47a35986ec19bad9d7f3165131127f5ade3bb516
Marko Čikoš <marko.cikos@liferay.com>
2022-07-13 15:50:36 +0200

    LPS-158216 SF

--- modules/apps/frontend-view-state/frontend-view-state-api/src/main/java/com/liferay/frontend/view/state/exception/NoSuchFrontendDataSetEntryException.java
+++ modules/apps/frontend-view-state/frontend-view-state-api/src/main/java/com/liferay/frontend/view/state/exception/NoSuchFrontendDataSetEntryException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.frontend.view.state.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -27,7 +28,9 @@ public class NoSuchFrontendDataSetEntryException extends NoSuchModelException {
 		super(msg);
 	}
 
-	public NoSuchFrontendDataSetEntryException(String msg, Throwable throwable) {
+	public NoSuchFrontendDataSetEntryException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
